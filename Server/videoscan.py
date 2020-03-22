58d03ba34f3d4009278a0e4016a9d39ec2d7d865
trevoryao <55645157+trevoryao@users.noreply.github.com>
2020-03-21 22:20:43 -0400

    save incidents

--- Server/videoscan.py
+++ Server/videoscan.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from imutils.video import VideoStream
 import face_recognition
 import argparse
@@ -15,10 +14,12 @@ ap.add_argument("-o", "--output", type=str,
 	help="path to output video")
 ap.add_argument("-y", "--display", type=int, default=1,
 	help="whether or not to display output frame to screen")
-ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+ap.add_argument("-d", "--detection-method", type=str, default="hog",
 	help="face detection model to use: either `hog` or `cnn`")
 ap.add_argument("-f", "--display-id", type=int, default=1,
 	help="whether or not to display names and frames on screen")
+ap.add_argument("-t", "--record-time", type=int, default=5,
+	help="time in minutes to record incidents")
 args = vars(ap.parse_args())
 
 # load the known faces and embeddings
@@ -30,6 +31,11 @@ print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 writer = None
 time.sleep(2.0)
+
+to_write = False
+
+# converting minutes to seconds in the record-time
+args["record_time"] *= 60
 
 # loop over frames from the video file stream
 while True:
@@ -48,13 +54,6 @@ while True:
 		model=args["detection_method"])
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
-
-	# if the video writer is None *AND* we are supposed to write
-	# the output video to disk initialize the writer
-	if writer is None and args["output"] is not None:
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(args["output"], fourcc, 20,
-			(frame.shape[1], frame.shape[0]), True)
 
     # loop over the facial embeddings
 	for encoding in encodings:
@@ -79,9 +78,10 @@ while True:
 			# of votes (note: in the event of an unlikely tie Python
 			# will select first entry in the dictionary)
 			name = max(counts, key=counts.get)
+
+			# can stop taking video
 		else:
-			writer.write(frame)
-            # Notify homeowner
+			to_write = True
 
 		# update the list of names
 		names.append(name)
@@ -101,10 +101,25 @@ while True:
 			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 				0.75, (0, 255, 0), 2)
 
+	# if the video writer is None *AND* we are supposed to write
+	# the output video to disk initialize the writer
+	if writer is None and args["output"] is not None and to_write == True:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		path = args["output"] + "incident" + time.strftime("_%b-%d-%Y_%H%M",
+			time.localtime()) + ".avi"
+		writer = cv2.VideoWriter(path, fourcc, 24,
+			(frame.shape[1], frame.shape[0]), True)
+		start = time.time()
+
+	# if the writer is not None, write the frame with recognized
+	# faces to disk
+	if writer is not None and time.time() - start < args["record_time"]:
+		writer.write(frame)
+
     # check to see if we are supposed to display the output frame to
 	# the screen
-	if args["display"] == 0:
-		cv2.imshow("Frame", frame)
+	if args["display"] > 0:
+		cv2.imshow("BetterBell", frame)
 		key = cv2.waitKey(1) & 0xFF
 		# if the `q` key was pressed, break from the loop
 		if key == ord("q"):
@@ -115,4 +130,5 @@ cv2.destroyAllWindows()
 vs.stop()
 # check to see if the video writer point needs to be released
 if writer is not None:
+	print("[INFO] Video saved in " + path)
 	writer.release()
