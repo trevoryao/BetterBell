2008f4b06d035a535006f93dce983ca2ca1e37f8
trevoryao <55645157+trevoryao@users.noreply.github.com>
2020-03-23 18:31:17 -0400

    colour change

--- Server/videoscan.py
+++ Server/videoscan.py
@@ -96,7 +96,7 @@ while True:
 			left = int(left * r)
 			# draw the predicted face name on the image
 			cv2.rectangle(frame, (left, top), (right, bottom),
-				(0, 255, 0), 2)
+				(255, 0, 0), 2)
 			y = top - 15 if top - 15 > 15 else top + 15
 			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 				0.75, (0, 255, 0), 2)
